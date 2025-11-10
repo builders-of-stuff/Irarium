770de8c28059089644f55c2cc9cba73cfd856b31
Kyle Truong <ktruong008@gmail.com>
2025-11-10 12:48:06 -0500

    remove paraglide for build errors fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
-import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    sveltekit(),
-    paraglide({
-      project: './project.inlang',
-      outdir: './src/lib/paraglide'
-    })
+    sveltekit()
   ]
 });
